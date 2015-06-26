b3f673025dc5cbbefa235a08a280a1996030ff18
seungdols <choiseungho0822@gmail.com>
2015-06-26 22:32:45 +0900

    타임라인 글 수정 페이지 만들기

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -1,16 +1,19 @@
 class WallController < ApplicationController
   def write
   end
-  def write_complete 
-    p = Post.new
-	p.name = params[:writer]
-	p.content = params[:content]
-	if p.save
-	  redirect_to = "wall/posts"
-	else
-	  flash[:alert] = p.errors[:content][0]
-	  redirect_to = back
-	end
+  def write_complete
+      p = Post.new
+      p.name = params[:writer]
+      p.content = params[:content]
+      if p.save
+        redirect_to "/wall/posts"
+      else
+        flash[:alert] = p.errors[:content][0]
+        redirect_to :back
+      end
+    end
+  def edit
+  	 @post_edit = Post.find(params[:id])
   end
   def posts
   	@posts = Post.all
